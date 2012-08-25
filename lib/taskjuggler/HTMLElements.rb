2e0525bd98ea0ef933f9b9eb4743ac5676566a4a
Jan-Marek Glogowski <glogow@fbihome.de>
2012-08-25 20:38:22 +0200

    Add base HTML element to class list.

--- lib/taskjuggler/HTMLElements.rb
+++ lib/taskjuggler/HTMLElements.rb
@@ -18,10 +18,10 @@ class TaskJuggler
   module HTMLElements
 
     # A list of supported HTML tags.
-    htmlTags = %w( a b body br code col colgroup div em frame frameset footer
-                   h1 h2 h3 head html hr meta p pre table td title tr )
+    htmlTags = %w( a b base body br code col colgroup div em frame frameset
+                   footer h1 h2 h3 head html hr meta p pre table td title tr )
     # A list of HTML tags that are self-closing.
-    closureTags = %w( area base basefont br hr input img link meta )
+    closureTags = %w( area base basefont br frame hr input img link meta )
 
     # For every HTML tag, we generate a class with the equivalent uppercase
     # name. This class is derived off of XMLElement. This makes creating HTML
